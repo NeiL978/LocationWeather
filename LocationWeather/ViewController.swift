bf4611ee9dddd9c112e1fbda2d8aaba7f7cfcc1f
NeiL978 <godblessbaby@gmail.com>
2016-07-01 18:46:17 +0800

    Initial Commit

---
+++ LocationWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationWeather
+//
+//  Created by Neil Liu on 2016/7/1.
+//  Copyright © 2016年 NeiL Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
